548aa9b8f9d93b6b7e63815af157825b60ac7b7b
Vinícios Barbosa <viniciosbarbosa.dev@gmail.com>
2025-05-30 17:07:21 -0300

    refactor(sidebar): update navigation links to use ROUTES and next/image

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,3 +1,7 @@
 export const ROUTES = {
-  signIn: "/auth/sign-in"
+  signIn: "/auth/sign-in",
+  profile: "/profile",
+  repositories: "/repos",
+  followingUsers: "/following",
+  followingRepositories: (username: string) => `/following/${username}/repos`
 }
