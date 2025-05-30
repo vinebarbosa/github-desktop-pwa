bb6f3aff12dd825af10e789dde57d8cc59ed2533
Vinícios Barbosa <viniciosbarbosa.dev@gmail.com>
2025-05-30 17:28:01 -0300

    refactor(auth): update sign-in URL path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import { Button } from "@/components/button";
 
 import { signIn } from "@/lib/auth"
+import { ROUTES } from "@/lib/routes";
 
 export default function SignInPage() {
   return (
     <form className="min-h-screen flex flex-col items-center justify-center gap-4"
       action={async () => {
         "use server"
-        await signIn("github", { redirect: true, redirectTo: "/" })
+        await signIn("github", { redirect: true, redirectTo: ROUTES.followingUsers })
       }}
     >
       <span className="text-sm font-medium tracking-[1%]">Entra com sua conta do Github</span>
