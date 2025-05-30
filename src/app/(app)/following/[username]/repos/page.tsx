d651df6a6e0425865e95f03255938106ebd4a8d2
Vinícios Barbosa <viniciosbarbosa.dev@gmail.com>
2025-05-30 17:21:23 -0300

    feat: add FollowingRepositoriesPage and BackButton component

---
+++ src/app/(app)/following/[username]/repos/page.tsx
@@ -0,0 +1,32 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/avatar';
+import { Header } from '@/components/header';
+import { getUser } from '@/http/get-user';
+import { auth } from '@/lib/auth';
+import { BackButton } from '../../../../../components/back-button';
+
+interface FollowingRepositoriesPageProps {
+  params: Promise<{
+    username: string;
+  }>;
+}
+
+export default async function FollowingRepositoriesPage({ params }: FollowingRepositoriesPageProps) {
+  const session = await auth();
+  const username = (await params).username;
+
+  const authorizationToken = session?.accessToken;
+
+  const user = await getUser({ username, authorizationToken });
+
+  return (
+    <div className="flex flex-1">
+      <Header className="flex-row">
+        <BackButton>{user.name}</BackButton>
+        <Avatar>
+          <AvatarImage src={user.avatar_url} />
+          <AvatarFallback className="text-regular">VC</AvatarFallback>
+        </Avatar>
+      </Header>
+    </div>
+  );
+}
