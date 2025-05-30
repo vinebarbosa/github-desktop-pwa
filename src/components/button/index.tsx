2f20be3ef688a2834c00fff073ad31ae353e26ef
Vinícios Barbosa <viniciosbarbosa.dev@gmail.com>
2025-05-30 06:31:17 -0300

    feat(button): add button component with variants and merging classes utility

---
+++ src/components/button/index.tsx
@@ -0,0 +1,17 @@
+import { mergeClasses } from '@/lib/tailwind';
+import { Slot } from '@radix-ui/react-slot';
+import type { VariantProps } from 'class-variance-authority';
+import { buttonVariants } from './button-variants';
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
+export function Button({ className, variant, size, asChild = false, ...props }: ButtonProps) {
+  const Component = asChild ? Slot : 'button';
+  return (
+    <Component className={mergeClasses(buttonVariants({ variant, size, className }))} {...props} />
+  );
+}
