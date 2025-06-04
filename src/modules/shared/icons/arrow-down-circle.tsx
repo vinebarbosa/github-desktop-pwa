6571f8f1dd17d395aaa192bca486848a6e5f8ae2
Vinícios Barbosa <viniciosbarbosa.dev@gmail.com>
2025-06-04 11:01:28 -0300

    refactor: implement mobile layout support
    Implements a new sidebar component for improved navigation and layout consistency.

---
+++ src/modules/shared/icons/arrow-down-circle.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export function ArrowDownCircleIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      width="24"
+      height="25"
+      viewBox="0 0 24 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M17 12.0779L12 17.0842L7 12.0779M12 6.07038V16.083V6.07038ZM12 1.06409C18.075 1.06409 23 5.99528 23 12.0779C23 18.1606 18.075 23.0918 12 23.0918C5.925 23.0918 1 18.1606 1 12.0779C1 5.99528 5.925 1.06409 12 1.06409Z"
+        stroke="currentColor"
+        strokeWidth="2"
+      />
+    </svg>
+  );
+}
